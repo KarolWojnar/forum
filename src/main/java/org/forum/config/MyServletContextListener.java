0761b2cf0017953b3c76f3978546331c0d75ab57
KarolWojnar <karolwojnar47@gmail.com>
2025-02-26 12:06:31 +0100

    synchronized session

--- src/main/java/org/forum/config/MyServletContextListener.java
+++ src/main/java/org/forum/config/MyServletContextListener.java
@@ -12,9 +12,9 @@ public class MyServletContextListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        log.info("Context initialized");
         Dotenv dotenv = Dotenv.load();
         System.setProperty("FORUM_MAIL_USERNAME", Objects.requireNonNull(dotenv.get("FORUM_MAIL_USERNAME")));
         System.setProperty("FORUM_MAIL_PASSWORD", Objects.requireNonNull(dotenv.get("FORUM_MAIL_PASSWORD")));
+        log.info("Context initialized");
     }
 }
